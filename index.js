8fc83442853f97e29b29ce8f47741221ec6373a0
Beka Bantasdze <thelataito@gmail.com>
2022-10-17 16:59:34 +0400

    test git

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ let button = document.getElementById("submit")
 button.addEventListener("click", () => {
     alert("თქვენ წარმატებით გაიარეთ რეგისტრაცია")
 })
+
+function myFunction() {
+  let x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
